d458361a7cb21367f3bc79d056c46e8d75594ef2
alejandro-filigrana <afiligranac@estudiante.uniajc.edu.co>
2024-09-30 20:36:32 +0000

    finalizacion de ejercicio 56

--- clase30sep2024/ejercicio56.java
+++ clase30sep2024/ejercicio56.java
@@ -1,7 +1,24 @@
-public class ejercicio56{
-    public static void main(String[] args){
+import java.util.Scanner;
 
-        System.out.println("mi primer programa de java en Github");
+public class ejercicio56 {
+    public static void main(String[] args) {
+        double nota = 0.0;
+        int aprobados = 0, noAprobados = 0;
+        // notas de los estudiantes
+        Scanner leer = new Scanner(System.in);
+        System.out.print("digite la cantidad de estudiantes:");
+        int estudiantes = leer.nextInt();
+        for (int i = 1; i <= estudiantes; i++) {
+            System.out.print("nota del estudiante " + i + "=");
+            nota = leer.nextDouble();
+            if (nota >= 5.0) {
+                aprobados++;
+            } else {
+                noAprobados++;
+
+            }
+        }
+        System.out.println("aprobados : " + aprobados + noAprobados + noAprobados);
 
     }
 }
